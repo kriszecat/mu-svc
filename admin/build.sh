a04bb5977b2f80383d88679178c5ff96f079b9f6
cchabot <cchabot@sodifrance.fr>
2016-04-15 17:35:48 +0200

    Corrections et ajout du build Docker dans Maven

--- admin/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-mvn clean package
-docker build -t hq/admin .
-docker tag hq/admin $(docker-machine ip registry):5000/hq/admin
-docker push $(docker-machine ip registry):5000/hq/admin
-docker rmi -f $(docker images -q hq/admin)
-docker rmi -f $(docker images -q java)
